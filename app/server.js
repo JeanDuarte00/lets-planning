91e4446b9395e6c8dfa5b68c8d7dcdf089d7f549
JeanDuarte00 <jean_luccas320@hotmail.com>
2023-01-27 22:57:10 -0300

    update application

--- app/server.js
+++ app/server.js
@@ -9,11 +9,12 @@ const maxNameLength = 30;
 const port = process.env.PORT || 3000;
 let users = [];
 let names = [];
+let cards = ['1','2','3','5','?'];
 
-const populateTeam = (teamList) => {
+const populateTeam = (list) => {
     console.log("populate")
-    console.log(teamList)
-    io.emit('team', teamList);
+    console.log(list)
+    io.emit('team', list);
 }
 
 app.get('/', (req, res) => {
@@ -24,11 +25,15 @@ app.get('/planning', (req, res) => {
     var params = req.query;
     console.log(params.user)
 
+
     // deve pegar o nome do usuario, associar com um ID(hash base64 do nome) no server e salvar num dicionario com o voto
     let user = {
         'name': params.user.toUpperCase(),
-        'vote': ''
+        'vote': '',
     }
+
+    console.log("YOU")
+    
     
     let data = names.indexOf(user.name);
     console.log(data)
@@ -39,21 +44,26 @@ app.get('/planning', (req, res) => {
         users.push(user);
         names.push(user.name);
     }
+    io.emit('you', user);
 
-    populateTeam(users)
-    
-    
     res.sendFile(__dirname + '/pages/planning/planning.html');
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    populateTeam(users)
+    populateTeam(users);
+    //populateCards(cards);
     //console.log(socket.handshake)
 
     socket.on('chat message', (msg) => {
         //console.log('message: ' + msg);
         io.emit('chat message', msg);
+    });
+
+
+    socket.on('card-options', (arg, callback) => {
+        //console.log('message: ' + msg);
+        callback(cards);
     });
 
    
--- build-run.sh
+++
@@ -1,8 +0,0 @@
-echo "=====================STOP APP============================"
-sudo docker stop lets-planning
-
-echo "=====================BUILDING============================"
-sudo docker build -t jeanduarte00/lets_planning .
-
-echo "=====================RUNNING============================="
-sudo docker run --rm -d -p 3000:3000 -v $(pwd)/:/usr/src/app --name lets_planning jeanduarte00/lets_planning
